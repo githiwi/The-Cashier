84d9b1aa30bcb2243f55288f283824fba1173e6b
hiwi-dci-student <hiwot.abdi@dci.education>
2023-01-09 18:12:04 +0100

    feat: cashCounter works for even changes and fixed issues from before

--- solution.js
+++ solution.js
@@ -1,39 +1,60 @@
 function cashCounter(price, cash) {
   let cashRegister = [];
-  if (cash > 50 && cash > price) {
-    let change = cash - price;
+  let change = cash - price;
+  if (change >= 50 && cash > price) {
     let result = { "50euro": Math.floor(change / 50) };
     cashRegister.push(result);
-    cash = change % 50;
+    change = change % 50;
     // console.log("why1", cash);
     // console.log("why1", change);
   }
 
-  if (cash > 20 && cash > price) {
+  //   if (cash > 20 && cash > price) {
+  //     //let change = cash - price;
+  //     let result = { "20euro": Math.floor(cash / 20) };
+  //     cashRegister.push(result);
+  //     cash = cash % 20;
+  //     console.log("why", cash);
+  //   }
+  if (change >= 20 && cash > price) {
     //let change = cash - price;
-    let result = { "20euro": Math.floor(cash / 20) };
+    let result = { "20euro": Math.floor(change / 20) };
     cashRegister.push(result);
-    cash = cash % 20;
-    //console.log("why", cash);
-  }
-  if (cash > 10 && cash > price) {
-    //let change = cash - price;
-    let result = { "10euro": Math.floor(cash / 10) };
-    cashRegister.push(result);
-    cash = cash % 10;
+    change = change % 20;
+    // console.log("why1", cash);
+    // console.log("why1", change);
   }
 
-  if (cash >= 5 && cash >= price) {
+  //   if (cash > 10 && cash > price) {
+  //     //let change = cash - price;
+  //     let result = { "10euro": Math.floor(cash / 10) };
+  //     cashRegister.push(result);
+  //     cash = cash % 10;
+  //   }
+  if (change >= 10 && cash > price) {
     //let change = cash - price;
-
-    let result = { "5euro": Math.floor(cash / 5) };
+    let result = { "10euro": Math.floor(change / 10) };
     cashRegister.push(result);
-    cash = cash % 5;
+    change = change % 10;
+    // console.log("100", cash);
+  }
+  //   if (cash >= 5 && cash >= price) {
+  //     //let change = cash - price;
+
+  //     let result = { "5euro": Math.floor(cash / 5) };
+  //     cashRegister.push(result);
+  //     cash = cash % 5;
+  //   }
+  if (change >= 5 && cash > price) {
+    //let change = cash - price;
+    let result = { "5euro": Math.floor(change / 5) };
+    cashRegister.push(result);
+    change = change % 5;
   }
   return cashRegister;
 }
-// console.log(cashCounter(5, 10)); //[ '{5 euro : 1}' ]
-// console.log(cashCounter(5, 25));
-// console.log(cashCounter(10, 20));
-//console.log(cashCounter(4.5, 20));
+console.log(cashCounter(5, 10)); //[ '{5 euro : 1}' ]
+console.log(cashCounter(5, 25));
+console.log(cashCounter(10, 20));
+console.log(cashCounter(4.5, 20));
 console.log(cashCounter(5, 100));
